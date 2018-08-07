a492cd621dfe6016a096542c732a0cc10f6cea20
Casey Zimmer <czimmer@ramapo.edu>
2018-08-07 17:34:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,10 @@ var playlist = { sum41: "fatlip"};
 
 function updatePlaylist(playlist, artist, songTitle)
 {
-  return Object.assign({},playlist,{[artist]: [songTitle]});
+  return Object.assign(playlist,{[artist]: [songTitle]});
+}
+
+function removeFromPlaylist(playlist,artist)
+{
+  return delete playlist[artist];
 }
